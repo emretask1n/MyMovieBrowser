a1dfafabeeccbaaff220548338556f6084c99377
emretask1n <emretaskn57@gmail.com>
2020-11-22 15:50:12 +0300

    Initial commit

---
+++ src/main/java/com/example/mymoviebrowser/DetailsActivity.java
@@ -0,0 +1,23 @@
+package com.example.mymoviebrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        Movie movie = getIntent().getParcelableExtra(name:"movie");
+
+        FragmentTransaction fts = getSupportFragmentManager().beginTransaction();
+        fts.add(R.id.container, DetailsFragment.newInstance(movie));
+        fts.commit();
+
+    }
+
+}
